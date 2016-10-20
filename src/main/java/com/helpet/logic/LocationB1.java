a569e5b31a773411f5f1a58eff9763bdedf0caa5
Carlos Agudelo <carloskikea@gmail.com>
2016-10-20 13:32:06 -0500

    add documentation in class

--- src/main/java/com/helpet/logic/LocationB1.java
+++ src/main/java/com/helpet/logic/LocationB1.java
@@ -10,8 +10,8 @@ import com.helpet.iw.exception.DaoException;
 
 /**
  * 
- * @author Carlos Enrique Agudelo Giraldo
- * @email carloskikea@gmail.com
+ * @author Carlos Enrique Agudelo Giraldo, Laura Martinez Londoño
+ * @email carloskikea@gmail.com, laumarlon@gmail.com
  * @description Interface for Location in lauyer of bussines
  * @date 10/17/206 
  *
@@ -20,7 +20,7 @@ import com.helpet.iw.exception.DaoException;
 public interface LocationB1 {
 	
 	/**
-	 * 
+	 * Metodo que registra la visualizacion de una mascota
 	 * @param userId
 	 * @param petId
 	 * @param latitude
@@ -29,12 +29,12 @@ public interface LocationB1 {
 	 * @param image
 	 * @param Observations
 	 * @param date
-	 * @throws DaoException
+	 * @throws DaoException manejo de excepciones a partir de clase personalida, donde se lleva el error a un log de auditoria
 	 */
 	void registrationLocation(int userId, int petId, float latitude, float longitude, String address, String image, String Observations, Date  date) throws DaoException;
 	
 	/**
-	 * 
+	 * Metodo realiza la actualización de la informacion de una mascota visualizada
 	 * @param id
 	 * @param userId
 	 * @param petId
@@ -44,23 +44,23 @@ public interface LocationB1 {
 	 * @param image
 	 * @param Observations
 	 * @param date
-	 * @throws DaoException
+	 * @throws DaoException manejo de excepciones a partir de clase personalida, donde se lleva el error a un log de auditoria
 	 */
 	void updateLocation(int id, int userId, int petId, float latitude, float longitude, String address, String image, String Observations, Date  date) throws DaoException;
 	
 	/**
-	 * 
+	 * Metodo que muetsra la informacion registrada en la visualizacion de una mascota vsualizada
 	 * @param id
 	 * @return
-	 * @throws DaoException
+	 * @throws DaoException manejo de excepciones a partir de clase personalida, donde se lleva el error a un log de auditoria
 	 */
 	Location showLocation(int id) throws DaoException;
 	
 	/**
-	 * 
+	 * MEtodo que lista los registros de una mascota visualizada
 	 * @param idPet
 	 * @return
-	 * @throws DaoException
+	 * @throws DaoException manejo de excepciones a partir de clase personalida, donde se lleva el error a un log de auditoria
 	 */
 	List<Location> ListLocationPet(int idPet) throws DaoException;
 	
